b8ad681a57dbac7da2d80bfc96d9bf3f09779097
Sam01 <148879548+okox01@users.noreply.github.com>
2026-03-13 12:55:01 +0600

    updated

--- 07_group_by.sql
+++ 07_group_by.sql
@@ -69,3 +69,29 @@ ORDER BY total_customers DESC;
 SELECT customer_id, SUM(total_amount) AS revenue
 FROM orders
 GROUP BY customer_id;
+
+
+
+--Corporate HQ is conducting a study on the relationship between replacement cost and a movie MPAA rating(e.g G,PG,R,etc...)
+--What is the average replacement cost per MPAA rating ?
+
+SELECT rating,ROUND(AVG(replacement_cost),3) FROM film
+GROUP BY rating
+ORDER BY ROUND(AVG(replacement_cost),3) DESC;
+
+
+
+--We aer running a promotion to reward our top 5 customers with coupons
+--What are the customer ids of the top 5 customers by total spend?
+SELECT customer_id,SUM(amount) FROM payment
+GROUP BY customer_id
+ORDER BY SUM(amount) DESC
+LIMIT 5;
+
+
+--WE have two  staff members,with staff IDS 1 and 2. WE want to give bonus to the 
+--staff member that handled the most payments
+--How many payments did each staff member handle and who gets the bonus?
+SELECT staff_id,COUNT(amount) FROM payment
+GROUP BY staff_id
+ORDER BY COUNT(amount) DESC;
