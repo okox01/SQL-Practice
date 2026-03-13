98995dcafeec22e9fb65bda84cdf16a3b64badb9
Sam01 <148879548+okox01@users.noreply.github.com>
2026-03-13 17:45:32 +0600

    having updated

--- 08_having.sql
+++ 08_having.sql
@@ -64,3 +64,19 @@ SELECT customer_id, SUM(total_amount) AS revenue
 FROM orders
 GROUP BY customer_id
 HAVING SUM(total_amount) > 400;
+
+--We are launching a platinum service for our most loyal customers
+--we will assign platinum status to customers that have had 40
+-- or more transaction payments
+--What customer_ids are eligible for platinum status?
+SELECT customer_id,COUNT(customer_id) FROM payment
+GROUP BY customer_id
+HAVING COUNT(customer_id)>=40;
+
+--What are the customer ids of customers who have spent more than 
+--100$ in payment transactions with our staff_id member 2 ?
+
+SELECT customer_id,staff_id,SUM(amount) FROM payment
+WHERE staff_id=2
+GROUP BY customer_id,staff_id
+HAVING SUM(amount)>100;
